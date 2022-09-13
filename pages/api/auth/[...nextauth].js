e553e091d8324eb227072a07ae96bb9652f4dc19
jimbolikesgithub <jamesedwards@Jamess-MacBook-Pro.local>
2022-09-13 15:32:01 -0400

    fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import TwitterProvider from "next-auth/providers/twitter"
-
+ 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
