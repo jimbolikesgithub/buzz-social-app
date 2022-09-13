d49abed2fa8ae9b3085bdf8bf9d71a854913f46a
jimbolikesgithub <jamesedwards@Jamess-MacBook-Pro.local>
2022-09-13 16:28:15 -0400

    fix

--- pages/api/addBuzz.ts
+++ pages/api/addBuzz.ts
@@ -28,7 +28,9 @@ export default async function handler(
     ]
   }
 
-  const apiEndpoint= `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-06-07/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`
+  // const apiEndpoint= `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-06-07/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`
+  
+  const apiEndpoint= `https://5gby2wvv.api.sanity.io/v2021-06-07/data/mutate/production`
 
   const result = await fetch(apiEndpoint, {
     headers: {
