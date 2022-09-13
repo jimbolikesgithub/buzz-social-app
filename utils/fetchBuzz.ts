4cad29a07dfdaaea8544ec7395ed1856531a4a7f
jimbolikesgithub <jamesedwards@Jamess-MacBook-Pro.local>
2022-09-13 12:58:02 -0400

    fix

--- utils/fetchBuzz.ts
+++ utils/fetchBuzz.ts
@@ -3,7 +3,7 @@ import { Buzz } from '../typings';
 
 export const fetchBuzz = async () => {
     // Backend endpoint
-    const res = await fetch(`${process.env.NEXTAUTH_URL}api/getBuzz`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/getBuzz`)
 
     const data = await res.json();
     const buzz: Buzz[] = data.buzz;
